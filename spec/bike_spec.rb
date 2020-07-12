30d5595e885357a382edae4edd530a3a4dd038a2
Matthew Lock <matthew_lock20@hotmail.com>
2020-07-12 10:43:27 +0100

    Updated readme

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -2,6 +2,8 @@ require 'bike'
 
 describe Bike do
   #it { is_expected.to respond_to :working? }
+  #alternative one liner syntax
+  
   #it "checks bike is working" do 
     #expect(subject.working?).to eq true
 
